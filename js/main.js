3f0b0215b243e46d6edb4d5b667b9f086ff1958d
Zhemchuzhnov Dmitriy <zhemchuzhnov.d@gmail.com>
2021-02-06 15:09:25 +0300

    fix like js listener

--- js/main.js
+++ js/main.js
@@ -1,22 +1,26 @@
 const menuBtn = document.querySelector('.menu-button');
-const menuNav = document.querySelector('nav');
-menuBtn.addEventListener('click', () => {
-    if (menuBtn.classList.contains('active')) {
-        menuBtn.classList.remove('active');
-        menuNav.classList.remove('nav-open');
-    } else {
-        menuBtn.classList.add('active');
-        menuNav.classList.add('nav-open');
-    }
-})
+if (menuBtn !== null) {
+    const menuNav = document.querySelector('nav');
+    menuBtn.addEventListener('click', () => {
+        if (menuBtn.classList.contains('active')) {
+            menuBtn.classList.remove('active');
+            menuNav.classList.remove('nav-open');
+        } else {
+            menuBtn.classList.add('active');
+            menuNav.classList.add('nav-open');
+        }
+    })
+}
 
 const likeBtn = document.querySelector('.like-btn');
-likeBtn.addEventListener('click', () => {
-    if (likeBtn.classList.contains('icon-heart-empty')) {
-        likeBtn.classList.remove('icon-heart-empty');
-        likeBtn.classList.add('icon-heart');
-    } else if (likeBtn.classList.contains('icon-heart')) {
-        likeBtn.classList.remove('icon-heart');
-        likeBtn.classList.add('icon-heart-empty');
-    }
-})
+if (likeBtn !== null) {
+    likeBtn.addEventListener('click', () => {
+        if (likeBtn.classList.contains('icon-heart-empty')) {
+            likeBtn.classList.remove('icon-heart-empty');
+            likeBtn.classList.add('icon-heart');
+        } else if (likeBtn.classList.contains('icon-heart')) {
+            likeBtn.classList.remove('icon-heart');
+            likeBtn.classList.add('icon-heart-empty');
+        }
+    })
+}
